0b00d1dae78e040da572c922ca3645cd0faa210e
Daniel Plaisted <dsplaisted@gmail.com>
2014-09-30 18:37:45 -0700

    Variable height rows

--- TwitterSearch.XamarinForms/TwitterSearch.XamarinForms/TwitterSearch.XamarinForms/ViewModels/SearchResultsViewModel.cs
+++ TwitterSearch.XamarinForms/TwitterSearch.XamarinForms/TwitterSearch.XamarinForms/ViewModels/SearchResultsViewModel.cs
@@ -53,6 +53,23 @@ namespace TwitterSearch.ViewModels
                 }
 
                 Tweets.Clear();
+
+                Tweets.Add(new Tweet()
+                    {
+                        Author = "dsplaisted",
+                        ProfileImageUrl = new Uri("https://pbs.twimg.com/profile_images/51934680/iguana_bigger.jpg"),
+                        Text = "Short tweet",
+                        Timestamp = DateTime.Now
+                    });
+
+                Tweets.Add(new Tweet()
+                {
+                    Author = "dsplaisted",
+                    ProfileImageUrl = new Uri("https://pbs.twimg.com/profile_images/51934680/iguana_bigger.jpg"),
+                    Text = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+                    Timestamp = DateTime.Now
+                });
+
                 foreach (var tweet in searchResponse.Content)
                 {
                     Tweets.Add(new Tweet()
--- TwitterSearch.XamarinForms/TwitterSearch.XamarinForms/TwitterSearch.XamarinForms/Views/SearchResultsView.cs
+++ TwitterSearch.XamarinForms/TwitterSearch.XamarinForms/TwitterSearch.XamarinForms/Views/SearchResultsView.cs
@@ -15,10 +15,12 @@ namespace TwitterSearch.Views
 
             var listView = new ListView
             {
-                RowHeight = 80
+                //RowHeight = 80
             };
-            listView.SetBinding(ListView.ItemsSourceProperty, "Tweets");
+
+            listView.HasUnevenRows = true;
             listView.ItemTemplate = new DataTemplate(typeof(TweetCell));
+            listView.SetBinding(ListView.ItemsSourceProperty, "Tweets");
 
             Content = listView;
         }
@@ -29,33 +31,78 @@ namespace TwitterSearch.Views
             {
                 var image = new Image
                 {
-                    HorizontalOptions = LayoutOptions.Start
+                    HorizontalOptions = LayoutOptions.Start,
+                    VerticalOptions = LayoutOptions.Start
                 };
                 image.SetBinding(Image.SourceProperty, new Binding("ProfileImageUrl"));
                 image.WidthRequest = image.HeightRequest = 40;
 
+
+                //var image = new BoxView()
+                //{
+                //    HorizontalOptions = LayoutOptions.Start,
+                //    VerticalOptions = LayoutOptions.Start,
+                //    BackgroundColor = Color.Red
+                //};
+                //image.WidthRequest = image.HeightRequest = 40;
+
+                //var imageBorder = new Frame()
+                //{
+                //    Content = image,
+                //    //Content = textLayout,
+                //    OutlineColor = Color.Red
+                //};
+
                 var user = new Label()
                 {
-                    HorizontalOptions = LayoutOptions.FillAndExpand
+                    HorizontalOptions = LayoutOptions.StartAndExpand,
+                    VerticalOptions = LayoutOptions.StartAndExpand,
+                    Font = Font.SystemFontOfSize(NamedSize.Large)
                 };
                 user.SetBinding(Label.TextProperty, "Author");
 
                 var tweetText = new Label()
                 {
-                    HorizontalOptions = LayoutOptions.FillAndExpand
+                    HorizontalOptions = LayoutOptions.StartAndExpand,
+                    VerticalOptions = LayoutOptions.StartAndExpand,
+                    //HeightRequest = 100
                 };
                 tweetText.SetBinding(Label.TextProperty, "Text");
 
                 var textLayout = new StackLayout()
                 {
                     Orientation = StackOrientation.Vertical,
+                    HorizontalOptions = LayoutOptions.StartAndExpand,
+                    VerticalOptions = LayoutOptions.StartAndExpand,
                     Children = { user, tweetText }
                 };
+
+                //var horizontal = new StackLayout()
+                //{
+                //    Orientation = StackOrientation.Horizontal,
+                //    HorizontalOptions = LayoutOptions.FillAndExpand,
+                //    VerticalOptions = LayoutOptions.StartAndExpand,
+                //    //MinimumHeightRequest = 100,
+                //    //MinimumWidthRequest = 100,
+                //    Children = { image, textLayout },
+                //    Padding = new Thickness(0, 5)
+                //};
+
+                //var textLayout2 = new StackLayout()
+                //{
+                //    Orientation = StackOrientation.Vertical,
+                //    HorizontalOptions = LayoutOptions.StartAndExpand,
+                //    VerticalOptions = LayoutOptions.StartAndExpand,
+                //    Children = { image, textLayout }
+                //};
+
+                
 
                 var viewLayout = new StackLayout()
                 {
                     Orientation = StackOrientation.Horizontal,
-                    Children = { image, textLayout },
+                    //HorizontalOptions = LayoutOptions.StartAndExpand,
+                    Children = { image, textLayout},
                     Padding = new Thickness(0, 5)
                 };
 
