07b3c04667e9c1e809e744f48b2f5ab832a3c655
Daniel Plaisted <dsplaisted@gmail.com>
2014-10-09 00:20:23 -0400

    Switch to defining UI views in XAML rathar than C#.

---
+++ TwitterSearch.XamarinForms/TwitterSearch.XamarinForms/TwitterSearch.XamarinForms/Views/SearchResultsView.xaml.cs
@@ -0,0 +1,15 @@
+﻿﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace TwitterSearch
+{	
+	public partial class SearchResultsView : ContentPage
+	{	
+		public SearchResultsView ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
+
